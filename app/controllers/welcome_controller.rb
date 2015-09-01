b7762aaaabb69388605bc4dfaf49c48c0c5cac9c
KoanBrown <koan.brown@gmail.com>
2015-09-01 12:59:56 -0700

    added search to main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
 
   def index
     @post = Post.last
+    @posts = Post.all
+    if params[:search]
+      @posts = Post.search(params[:search]).order("created_at DESC")
+    end
   end
 
 end
