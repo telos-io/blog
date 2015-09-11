aecb0eabdc817a878953e460baccb02b3cea8732
KoanBrown <koan.brown@gmail.com>
2015-09-11 10:08:23 -0700

    initialized user permisions tech

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :manage, :all if user.role == "poster"
-    can :create, Comment if user.role == "commenter"
+    can :manage, :all if user.role == "admin"
+    can :create, Comment if user
     can :read, :all
 
   end
