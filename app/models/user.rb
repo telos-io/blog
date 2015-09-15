6b4a69fa0522a44fec809a0150046d76048f2269
KoanBrown <koan.brown@gmail.com>
2015-09-15 12:21:36 -0700

    styling changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   def admin?
    self.role.name == "admin"
   end
