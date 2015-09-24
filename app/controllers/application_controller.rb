8661dedbe5ba6341d8e682e366451784152b8161
KoanBrown <koan.brown@gmail.com>
2015-09-23 19:14:59 -0700

    styling and layout changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user
+    @user = current_user
+  end
+
 end
