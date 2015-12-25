911d66eac22f3e539a57a40844652f0a95ad0e13
KoanBrown <koan.brown@gmail.com>
2015-12-24 19:09:37 -0800

    working on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
     @post = Post.last
-    @posts = Post.all
-    if params[:search]
-      @posts = Post.search(params[:search]).order("created_at DESC")
-    end
   end
 
 end
